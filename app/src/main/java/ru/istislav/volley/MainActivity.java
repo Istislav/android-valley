48c14bc3038ec013fe4a1753e016e337b18371ed
CyberUnit <cyberunit@gmail.com>
2020-08-19 14:53:33 +0300

    getting JSON objects

--- app/src/main/java/ru/istislav/volley/MainActivity.java
+++ app/src/main/java/ru/istislav/volley/MainActivity.java
@@ -12,6 +12,7 @@ import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.VolleyLog;
 import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 
@@ -23,6 +24,7 @@ public class MainActivity extends AppCompatActivity {
 
     private final static String URL = "https://api.reelgood.com/v3.0/content/people/best-of?availability=onAnySource&content_kind=both&hide_seen=false&hide_tracked=false&hide_watchlisted=false&imdb_end=10&imdb_start=0&region=us&rg_end=100&rg_start=0&sort=0&year_end=2020&year_start=1900";
     private final static String URL_String = "https://api.reelgood.com/v3.0/checkip";
+    private final static String URL_EQ = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/2.5_day.geojson";
 
     private RequestQueue queue;
 
@@ -33,8 +35,36 @@ public class MainActivity extends AppCompatActivity {
 
         queue = Volley.newRequestQueue(this);
         getStringObject(URL_String);
+        getJsonObject(URL_EQ);
         getArrayObject(URL);
 
+    }
+
+    public void getJsonObject(String url) {
+        JsonObjectRequest objectRequest = new JsonObjectRequest(Request.Method.GET, url, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+                try {
+                    Log.d("Object string: ", response.getString("type").toString());
+                    JSONObject metadata = response.getJSONObject("metadata");
+                    Log.d("Object subobject", metadata.toString());
+                    Log.d("Object subobject Str", metadata.getString("title").toString());
+
+                    JSONArray features = response.getJSONArray("features");
+                    JSONObject propObj = features.getJSONObject(0).getJSONObject("properties");
+                    Log.d("Object arr Str", propObj.getString("place").toString());
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                VolleyLog.d("Error", error.getMessage());
+            }
+        });
+        queue.add(objectRequest);
     }
 
     public void getArrayObject(String url) {
@@ -70,7 +100,7 @@ public class MainActivity extends AppCompatActivity {
         }, new Response.ErrorListener() {
             @Override
             public void onErrorResponse(VolleyError error) {
-
+                VolleyLog.d("Error", error.getMessage());
             }
         });
         queue.add(stringRequest);
