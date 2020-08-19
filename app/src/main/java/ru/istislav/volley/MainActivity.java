ac0ae3d98fe9e77d29127aff8562dbe3c4ccac2e
CyberUnit <cyberunit@gmail.com>
2020-08-19 14:24:18 +0300

    getting JSON string and JSON array

--- app/src/main/java/ru/istislav/volley/MainActivity.java
+++ app/src/main/java/ru/istislav/volley/MainActivity.java
@@ -12,6 +12,7 @@ import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.VolleyLog;
 import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 
 import org.json.JSONArray;
@@ -21,16 +22,24 @@ import org.json.JSONObject;
 public class MainActivity extends AppCompatActivity {
 
     private final static String URL = "https://api.reelgood.com/v3.0/content/people/best-of?availability=onAnySource&content_kind=both&hide_seen=false&hide_tracked=false&hide_watchlisted=false&imdb_end=10&imdb_start=0&region=us&rg_end=100&rg_start=0&sort=0&year_end=2020&year_start=1900";
+    private final static String URL_String = "https://api.reelgood.com/v3.0/checkip";
+
+    private RequestQueue queue;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        RequestQueue queue = Volley.newRequestQueue(this);
+        queue = Volley.newRequestQueue(this);
+        getStringObject(URL_String);
+        getArrayObject(URL);
 
+    }
+
+    public void getArrayObject(String url) {
         // because in the URL it is array, not object, else you need to use JsonObjectRequest
-        JsonArrayRequest arrayRequest = new JsonArrayRequest(Request.Method.GET, URL, new Response.Listener<JSONArray>() {
+        JsonArrayRequest arrayRequest = new JsonArrayRequest(Request.Method.GET, url, new Response.Listener<JSONArray>() {
             @Override
             public void onResponse(JSONArray response) {
 //                Log.d("Response", response.toString());
@@ -50,5 +59,20 @@ public class MainActivity extends AppCompatActivity {
             }
         });
         queue.add(arrayRequest);
+    }
+
+    public void getStringObject(String url) {
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                Log.d("My String: ", response.toString());
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+            }
+        });
+        queue.add(stringRequest);
     }
 }
